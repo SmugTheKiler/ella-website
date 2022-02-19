c050519c761a99f3b3c89c0bac2ec0fe9dcc3859
SmugTheKiler <emirgamaleev2009@gmail.com>
2022-02-18 20:00:35 -0500

    Delete log.js

--- log.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-function makeLog(data) {
-    fs.appendFile('../log.txt', `[${new Date().toLocaleString()}] ${data}\n`, (err) => {});
-    console.log(`New user loged in! | IP: ${data}`)
-}
-
-module.exports = makeLog; 
